002273d31c8db4d96daf7bd9444f72dc01668784
Gilberto Ribeiro <gillbertosr5@gmail.com>
2022-08-02 16:12:03 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Declarations
+const mobileBars = document.getElementById("mobileBars");
+
+// Events
+mobileBars.addEventListener("click", toggleMenu);
+
+// Functions
+function toggleMenu() {
+  const navList = document.querySelector(".nav-list");
+  navList.classList.toggle("active");
+}
