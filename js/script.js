37ac8169aad3762d4727f0941696d4998b266a7c
Gilberto Ribeiro <gillbertosr5@gmail.com>
2022-08-04 18:19:40 -0300

    Added Projects Function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-// import projects from "./projects";
+const projects = [
+  {
+    name: "Music Player",
+    url: "https://github.com/Gilbertosr5/MusicPlayer",
+    img: "img/projects/MusicPlayer.PNG",
+    text: "A simple Music Player system. With this you can listen your favorites songs and more! principal Language: Javascript",
+    category: "Web App",
+  },
+  {
+    name: "UNIP Clone",
+    url: "https://github.com/Gilbertosr5/UnipAluno-Clone",
+    img: "img/projects/clone_unip.PNG",
+    text: "A clone of UNIP Student Page, and this project have a simple login validation with Local Storage. Principal Language: CSS",
+    category: "Web App",
+  },
+  {
+    name: "Facebook Clone",
+    url: "https://github.com/Gilbertosr5/FaceLoginClone",
+    img: "img/projects/faceClone.PNG",
+    text: "A Very simple Facebook Clone. It's just a single page & I used Bootstrap v5.1 to do it.",
+    category: "Web App",
+  },
+];
 
 // Declarations
 const mobileBars = document.getElementById("mobileBars");
@@ -11,3 +33,27 @@ function toggleMenu() {
   const navList = document.querySelector(".nav-list");
   navList.classList.toggle("active");
 }
+
+function addProject() {
+  console.log("==============================================");
+  console.log("Adding Projects to Projects Area");
+  console.log("----------------------------------------------");
+  for (let i in projects) {
+    document.querySelector(".cards").innerHTML += `<div class="card">
+    <div class="card-img">
+      <img src="${projects[i].img}" alt="${projects[i].name}" />
+      <p class="card-name">${projects[i].name}</p>
+    </div>
+    <div class="card-text">
+      <p>
+        ${projects[i].text}
+      </p>
+      <a href="${projects[i].url}">See on GitHub</a>
+    </div>
+    </div>`;
+    console.log(`${projects[i].name} added`);
+  }
+  console.log("==============================================");
+}
+
+addProject();
