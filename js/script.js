9bdb2a0d98a390c3959ccc4a64d03eabeb39f11b
Gilberto Ribeiro <gillbertosr5@gmail.com>
2022-08-04 18:47:05 -0300

    Fixes on JS Projects

--- js/script.js
+++ js/script.js
@@ -4,26 +4,27 @@ const projects = [
     url: "https://github.com/Gilbertosr5/MusicPlayer",
     img: "img/projects/MusicPlayer.PNG",
     text: "A simple Music Player system. With this you can listen your favorites songs and more! principal Language: Javascript",
-    category: "Web App",
+    category: "web",
   },
   {
     name: "UNIP Clone",
     url: "https://github.com/Gilbertosr5/UnipAluno-Clone",
     img: "img/projects/clone_unip.PNG",
     text: "A clone of UNIP Student Page, and this project have a simple login validation with Local Storage. Principal Language: CSS",
-    category: "Web App",
+    category: "web",
   },
   {
     name: "Facebook Clone",
     url: "https://github.com/Gilbertosr5/FaceLoginClone",
     img: "img/projects/faceClone.PNG",
     text: "A Very simple Facebook Clone. It's just a single page & I used Bootstrap v5.1 to do it.",
-    category: "Web App",
+    category: "web",
   },
 ];
 
 // Declarations
 const mobileBars = document.getElementById("mobileBars");
+const body = document.getElementsByTagName("body");
 
 // Events
 mobileBars.addEventListener("click", toggleMenu);
@@ -34,26 +35,79 @@ function toggleMenu() {
   navList.classList.toggle("active");
 }
 
-function addProject() {
-  console.log("==============================================");
-  console.log("Adding Projects to Projects Area");
-  console.log("----------------------------------------------");
-  for (let i in projects) {
-    document.querySelector(".cards").innerHTML += `<div class="card">
-    <div class="card-img">
-      <img src="${projects[i].img}" alt="${projects[i].name}" />
-      <p class="card-name">${projects[i].name}</p>
-    </div>
-    <div class="card-text">
-      <p>
-        ${projects[i].text}
-      </p>
-      <a href="${projects[i].url}">See on GitHub</a>
-    </div>
-    </div>`;
-    console.log(`${projects[i].name} added`);
+function addProject(btn) {
+  document.querySelector(".cards").innerHTML = "";
+  if (btn == "all") {
+    for (let i in projects) {
+      document.querySelector(".cards").innerHTML += `<div class="card">
+      <div class="card-img">
+        <img src="${projects[i].img}" alt="${projects[i].name}" />
+        <p class="card-name">${projects[i].name}</p>
+      </div>
+      <div class="card-text">
+        <p>
+          ${projects[i].text}
+        </p>
+        <a href="${projects[i].url}">See on GitHub</a>
+      </div>
+      </div>`;
+      console.log(`${projects[i].name} added`);
+    }
+  } else if (btn == "react") {
+    for (let i in projects) {
+      if (projects[i].category == btn) {
+        document.querySelector(".cards").innerHTML += `<div class="card">
+          <div class="card-img">
+            <img src="${projects[i].img}" alt="${projects[i].name}" />
+            <p class="card-name">${projects[i].name}</p>
+          </div>
+          <div class="card-text">
+            <p>
+              ${projects[i].text}
+            </p>
+            <a href="${projects[i].url}">See on GitHub</a>
+          </div>
+        </div>`;
+        console.log(`${projects[i].name} added`);
+      }
+    }
+  } else if (btn == "web") {
+    for (let i in projects) {
+      if (projects[i].category == btn) {
+        document.querySelector(".cards").innerHTML += `<div class="card">
+          <div class="card-img">
+            <img src="${projects[i].img}" alt="${projects[i].name}" />
+            <p class="card-name">${projects[i].name}</p>
+          </div>
+          <div class="card-text">
+            <p>
+              ${projects[i].text}
+            </p>
+            <a href="${projects[i].url}">See on GitHub</a>
+          </div>
+        </div>`;
+        console.log(`${projects[i].name} added`);
+      }
+    }
+  } else if (btn == "ux") {
+    for (let i in projects) {
+      if (projects[i].category == btn) {
+        document.querySelector(".cards").innerHTML += `<div class="card">
+          <div class="card-img">
+            <img src="${projects[i].img}" alt="${projects[i].name}" />
+            <p class="card-name">${projects[i].name}</p>
+          </div>
+          <div class="card-text">
+            <p>
+              ${projects[i].text}
+            </p>
+            <a href="${projects[i].url}">See on GitHub</a>
+          </div>
+        </div>`;
+        console.log(`${projects[i].name} added`);
+      }
+    }
   }
-  console.log("==============================================");
 }
 
 addProject();
